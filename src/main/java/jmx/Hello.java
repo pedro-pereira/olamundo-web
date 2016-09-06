4ce9e9aad7e0516450c840d54b0265eac4c43825
pedro-pereira-2 <pedrohenri.pereira@yahoo.com.br>
2016-09-06 17:06:14 -0300

    Alteracao de tipo de mensagem

--- src/main/java/jmx/Hello.java
+++ src/main/java/jmx/Hello.java
@@ -15,27 +15,28 @@ import org.springframework.jmx.export.annotation.ManagedResource;
 @ManagedResource(objectName="jmx:name=JMXHello", description="Teste de JMX")
 	public class Hello implements HelloMBean {
    
-   private String message = null;
+   private int message = 100;
 
    public Hello() {
-      message = "Hello, world";
+      message = 50;
    }
 
-   public Hello(String message) {
+   public Hello(int message) {
       this.message = message;
    }
 
-   public void setMessage(String message) {
+   public void setMessage(int message) {
       this.message = message;
    }
 
    @ManagedAttribute
-   public String getMessage() {
+   public int getMessage() {
       return message;
    }
 
    @ManagedOperation
-   public void sayHello() {
+   public int sayHello() {
       System.out.println(message);
+      return message;
    }
 }
